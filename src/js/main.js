c6c3bc05110a8cbca3205fcc2145b778955cc09a
Caffee <PascalCoffeeLake@gmail.com>
2022-10-26 21:28:03 +0900

    Add: Format

--- src/js/main.js
+++ src/js/main.js
@@ -2,41 +2,50 @@
 // HTMLが準備できたら、中身の記述を実行する
 // ここにjQueryの処理を記述
 
-$(function() {
+$(function () {
   var h = $(window).height(); //windowの高さ(px) 1vh = h/100(px);
 
   //フェードイン
-  $('#container > section').not('#mv').each(function () {
-    //タイトルのアニメーション
-    var windowWidth = window.innerWidth;
-    function beforeSectionRevealed(_this) {
-      $(_this).children('*').css('visibility', 'hidden');
-      $(_this).children('*').css('opacity', '1');
-    }
-    function afterSectionRevealed (_this) {
-      $(_this).find('.main-ttl-letters span').addClass('ttl-anime-letters');
-      $(_this).find('.main-ttl-line').addClass('ttl-anime-line');
-      $(_this).children('*').each(function(i) {
-      });
-      $(_this).removeAttr('style');
-    }
-    function afterSectionReseted (_this) {
-      $(_this).find('.main-ttl-letters span').removeClass('ttl-anime-letters');
-      $(_this).find('.main-ttl-line').removeClass('ttl-anime-line');
-    }
-  });
+  $('#container > section')
+    .not('#mv')
+    .each(function () {
+      //タイトルのアニメーション
+      var windowWidth = window.innerWidth;
+      function beforeSectionRevealed(_this) {
+        $(_this).children('*').css('visibility', 'hidden');
+        $(_this).children('*').css('opacity', '1');
+      }
+      function afterSectionRevealed(_this) {
+        $(_this).find('.main-ttl-letters span').addClass('ttl-anime-letters');
+        $(_this).find('.main-ttl-line').addClass('ttl-anime-line');
+        $(_this)
+          .children('*')
+          .each(function (i) {});
+        $(_this).removeAttr('style');
+      }
+      function afterSectionReseted(_this) {
+        $(_this).find('.main-ttl-letters span').removeClass('ttl-anime-letters');
+        $(_this).find('.main-ttl-line').removeClass('ttl-anime-line');
+      }
+    });
 
   //再読み込み時も表示
   jQuery(window).scroll();
 
   //ページ内リンクへのスムーズスクロール
-  $('a[href^="#"]').click(function(){
+  $('a[href^="#"]').click(function () {
     var speed = 1000;
     var header_h = 150; //px
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
+    var href = $(this).attr('href');
+    var target = $(href == '#' || href == '' ? 'html' : href);
     var position = target.offset().top - header_h;
-    $("html, body").animate({scrollTop: position}, speed, "easeOutExpo");
+    $('html, body').animate(
+      {
+        scrollTop: position,
+      },
+      speed,
+      'easeOutExpo'
+    );
     return false;
   });
 
@@ -47,13 +56,12 @@ $(function() {
     $('.header-nav').toggleClass('open');
   });
 
-  $(document).click(function(event){
+  $(document).click(function (event) {
     var target = $(event.target);
-    if(target.hasClass('hamburger-wrap')) {
+    if (target.hasClass('hamburger-wrap')) {
       target.fadeOut();
       $('.hamburger').removeClass('open');
       $('.header-nav').removeClass('open');
     }
   });
-
 });
