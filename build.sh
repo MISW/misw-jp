b3647c9638f9801fa6419afffb330f2bd3a2a79b
Caffee <PascalCoffeeLake@gmail.com>
2022-11-13 22:48:08 +0900

    Templating (#21)
    YAML ファイルを書き換えるだけで、幹部とバナーの更新ができるように

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
+
 npm install -g pnpm
-pnpm i --frozen-lockfile --dev
-pnpm sass:build
-mv -v src/ dist/
+pnpm i --frozen-lockfile
+pnpm build
 mv -v _headers dist/
 mv -v _redirects dist/
+
+exit
